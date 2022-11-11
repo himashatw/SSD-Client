cb374cd8fc1dc4ab5c81ff08e852e4fd739a39c0
Himasha Weerasinghe <himashatw@gmail.com>
2022-11-11 19:54:41 +0530

    router paths added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import Login from "./screens/Login";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import MessageUpload from "./screens/MessageUpload";
+import FileUpload from "./screens/FileUpload";
 
 function App() {
-  return <Login />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route exact path="/messages">
+          <MessageUpload />
+        </Route>
+        <Route exact path="/files">
+          <FileUpload />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
