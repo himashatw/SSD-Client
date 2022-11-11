0f09673dd60a1d1ad69dc5152eeec93a7fb3159c
Himasha Weerasinghe <himashatw@gmail.com>
2022-11-11 19:54:47 +0530

    loading added

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <div className="animate-spin rounded-full h-6 w-6 border-b-8 border-2 border-green-400 m-auto" />
+  );
+}
+
+export default Loading;
