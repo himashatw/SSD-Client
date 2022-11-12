8e77a713d9290ce89ce16bcdfd30d7f557f9542e
Himasha Weerasinghe <himashatw@gmail.com>
2022-11-12 22:42:52 +0530

    updated

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,6 +1,57 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
+
+import { Link, useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigation = useNavigate();
+
+  const {
+    loginWithRedirect,
+    loginWithPopup,
+    logout,
+    user,
+    isAuthenticated,
+    getAccessTokenSilently,
+  } = useAuth0();
+
+  const callAPI = () => {
+    axios
+      .get("http://localhost:8080/")
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  };
+
+  const protectedAPI = async () => {
+    const token = await getAccessTokenSilently();
+
+    try {
+      const res = await axios.get("http://localhost:8080/protected", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log(res.data);
+    } catch (error) {
+      console.log(error.message);
+    }
+
+    return;
+    // console.log(token);
+
+    await axios
+      .get("http://localhost:8080/protected", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  };
+
+  // isAuthenticated && navigation("/messages");
+
   return (
     <div>
       <p className="text-center mt-10 text-xl font-bold">Login</p>
@@ -37,10 +88,55 @@ function Login() {
           <button
             className="bg-blue hover:bg-blue-dark text-black font-bold py-2 px-4 rounded bg-cyan-100"
             type="button"
+            onClick={loginWithPopup}
           >
-            Sign In
+            Sign In with Popup
           </button>
         </div>
+        <div className="flex items-center justify-center">
+          <button
+            className="bg-blue hover:bg-blue-dark text-black font-bold py-2 px-4 rounded bg-cyan-100"
+            type="button"
+            onClick={loginWithRedirect}
+          >
+            Sign In with Redirect
+          </button>
+        </div>
+        <div className="flex items-center justify-center">
+          <button
+            className="bg-blue hover:bg-blue-dark text-black font-bold py-2 px-4 rounded bg-cyan-100"
+            type="button"
+            onClick={logout}
+          >
+            Log out
+          </button>
+        </div>
+
+        {isAuthenticated && <h3>user is {JSON.stringify(user, null, 2)}</h3>}
+        <h3>
+          user is{" "}
+          {isAuthenticated ? (
+            <span className="text-green-500">Authenticated</span>
+          ) : (
+            <span className="text-red-500">Not Authenticated</span>
+          )}
+        </h3>
+        <ul>
+          <li>
+            <button onClick={callAPI}>Call API</button>
+          </li>
+          <li>
+            <button onClick={protectedAPI}>Call protect API</button>
+          </li>
+        </ul>
+
+        <button
+          onClick={() => {
+            navigation("/messages");
+          }}
+        >
+          Call MSGS
+        </button>
       </div>
     </div>
   );
--- src/screens/MessageUpload.jsx
+++ src/screens/MessageUpload.jsx
@@ -1,12 +1,60 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 import Loading from "../components/Loading";
+import axios from "axios";
 
 function MessageUpload() {
+  const navigation = useNavigate();
+
+  const {
+    loginWithRedirect,
+    loginWithPopup,
+    logout,
+
+    user,
+    isAuthenticated,
+    getAccessTokenSilently,
+  } = useAuth0();
+
+  React.useLayoutEffect(() => {
+    return () => {
+      if (!isAuthenticated) {
+        navigation("/");
+      }
+    };
+  }, []);
+
+  const test = async () => {
+    const token = await getAccessTokenSilently();
+
+    // console.log(token);
+
+    axios
+      .get("http://localhost:8080/worker-validate", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+
+    // console.log(getAccessTokenSilently);
+  };
+
   return (
     <div>
       <p className="text-center mt-10 text-xl font-bold">
         Message Upload (Workers Portal)
       </p>
+      <button
+        onClick={() => {
+          logout();
+          navigation("/");
+        }}
+      >
+        Log out
+      </button>
       <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col mt-10 w-2/3 mx-auto my-auto">
         <div className="mb-4">
           <label
@@ -27,6 +75,7 @@ function MessageUpload() {
           <button
             className="bg-blue hover:bg-blue-dark text-black font-bold py-2 px-4 rounded bg-cyan-100"
             type="button"
+            onClick={test}
           >
             Upload
           </button>
