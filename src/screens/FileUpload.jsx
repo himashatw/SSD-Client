22e39f1b53432210923e2b4fbb54a85986da6912
Himasha Weerasinghe <himashatw@gmail.com>
2022-11-15 19:51:36 +0530

    core functionalities done

--- src/screens/FileUpload.jsx
+++ src/screens/FileUpload.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Toaster, toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function FileUpload() {
   const { logout, isAuthenticated, user, getAccessTokenSilently } = useAuth0();
   const navigation = useNavigate();
   const [uploads, setUploads] = React.useState("");
+
+  const msgText = React.useRef();
 
   const validateAccess = async () => {
     if (!isAuthenticated) {
@@ -30,23 +33,63 @@ function FileUpload() {
       return;
     }
 
-    // const token = await user.getIdTokenClaims();
-
     const token = await getAccessTokenSilently();
 
     e.preventDefault();
     const formData = new FormData();
     formData.append("email", user.email);
     formData.append("file", uploads);
-    const response = await fetch("http://localhost:8080/manager-validate", {
-      method: "POST",
-      body: formData,
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    const data = await response.json();
-    console.log(data);
+
+    try {
+      const response = await fetch("http://localhost:8080/manager-validate", {
+        method: "POST",
+        body: formData,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = await response.json();
+      const notificationMessage = data.message || "File uploaded successfully";
+      toast.success(notificationMessage);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  const onClickHandler = async () => {
+    if (msgText.current.value === "") {
+      toast.error("Please enter a message");
+      return;
+    }
+
+    const token = await getAccessTokenSilently();
+
+    axios
+      .post(
+        "http://localhost:8080/manager-validate-msg",
+        {
+          message: msgText.current.value,
+          email: user.email,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((res) => {
+        const responseMessage = res.data.message || "Successfull";
+        toast.success(responseMessage);
+        msgText.current.value = "";
+        console.log(res.data);
+      })
+      .catch((err) => {
+        const errorMsg = err.response.data.message || "Something went wrong";
+        toast.error(errorMsg);
+        console.log(err);
+      });
   };
 
   return (
@@ -87,6 +130,34 @@ function FileUpload() {
             className="bg-blue hover:bg-blue-dark text-black font-bold py-2 px-4 rounded bg-cyan-100"
             type="button"
             onClick={uploadFile}
+          >
+            Upload
+          </button>
+        </div>
+      </div>
+
+      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col mt-10 w-2/3 mx-auto my-auto">
+        <div className="mb-4">
+          <label
+            className="block text-grey-darker text-sm font-bold mb-2"
+            htmlFor="message"
+          >
+            Message
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+            id="message"
+            type="text"
+            placeholder="type here"
+            ref={msgText}
+          />
+        </div>
+
+        <div className="flex items-center justify-center">
+          <button
+            className="bg-blue hover:bg-blue-dark text-black font-bold py-2 px-4 rounded bg-cyan-100"
+            type="button"
+            onClick={onClickHandler}
           >
             Upload
           </button>
